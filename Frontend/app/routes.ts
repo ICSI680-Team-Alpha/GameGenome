0a9c14ea2c7c074c5efd910449d297fbbffe3bea
Alex <alexbailey791@gmail.com>
2025-04-06 14:57:57 -0400

    feat: add login route and component with styling

--- Frontend/app/routes.ts
+++ Frontend/app/routes.ts
@@ -1,3 +1,6 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("Login", "routes/logInRoute.tsx"), // Add this line for the /Login route
+] satisfies RouteConfig;
