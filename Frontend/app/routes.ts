7fbb369a93d69790fb6f2b27872928b7e438df4b
Alex <alexbailey791@gmail.com>
2025-04-09 10:48:15 -0400

    Add Account and Favorites components with styling and routing

--- Frontend/app/routes.ts
+++ Frontend/app/routes.ts
@@ -6,5 +6,6 @@ export default [
     route("Signup", "routes/signUpRoute.tsx"),
     route("Favorites", "routes/favoriteRoute.tsx"),
     route("Quiz", "routes/quizRoute.tsx"),
+    route("Account", "routes/accountRoute.tsx"),
     // route("Recommendations", "routes/recommendationsRoute.tsx")
 ] satisfies RouteConfig;
