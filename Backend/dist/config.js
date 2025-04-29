5655e3e5638fae5eeef3079b5992748dfff8b3ba
MoGHenry <moghenry0@gmail.com>
2025-04-29 17:26:45 -0400

    remove dist folder

--- Backend/dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    port: 5000,
-    mongodbUri: 'mongodb+srv://shelleymassinga:BencaodeDeus2025@cluster0.figpbbf.mongodb.net/Gamegenome?retryWrites=true&w=majority&appName=Cluster0',
-    nodeEnv: 'development'
-};
